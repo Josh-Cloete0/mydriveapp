da31a5555305324df0a986f4ba296d6f6a90011c
Josh-Cloete0 <st10473468@vcconnect.edu.za>
2025-09-04 14:53:40 +0200

    Added heading with custom styles

--- App.tsx
+++ App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { StyleSheet, Text, View,Image,SafeAreaView } from 'react-native';
 
 export default function App() {
-const age = 16;
+const age = 20;
 const licenseAge = 0;
-let feedback = '';
+let feedback  = '';//let means that the variable can hold any data type
 if (age >= 18) 
   if (licenseAge >= 3){
   
@@ -29,9 +29,15 @@ if (age >= 18)
 
   return (
     <SafeAreaView style={styles.container}>
-    <View >
-      <Image style={styles.Image} source ={{uri:'https://images.pexels.com/photos/430205/pexels-photo-430205.jpeg'}} />
-     <Text style={styles.response}>
+    <View style={styles.content}>
+    <Text style={[styles.response,{fontSize:48,fontWeight:'bold',marginBottom:35,textAlign:'center',color:'white'}]}>
+      Driving License Checker</Text>{/*custom style and other text component specific styles to the heading */}
+      {/*Image component to display an image from the web */}
+      <Image style={styles.Image} 
+      source ={{uri:'https://images.pexels.com/photos/430205/pexels-photo-430205.jpeg'}} />
+      <Text style={styles.response}>Age:{age}</Text>
+      <Text style={styles.response}>License Age:{licenseAge}</Text>
+     <Text style={[styles.response,{fontSize:48,fontWeight:'bold',marginBottom:35,textAlign:'center',color:'blue'}]}>
       {feedback}</Text>{/*This is my message to the user*/}
       
     </View>
@@ -47,16 +53,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
+  content:{
+    width:'100%',
+    alignItems:'center',
+  },
+
   response:{
     fontSize:28,
     color:"black",
+    marginTop:5,
+    fontWeight:'bold',
 
   },
 
   Image:{
-    width:300,
-    height:300,
-    resizeMode:'contain'
+    width:"100%",
+    height:"50%",
+    resizeMode:'contain',
+    top:0
   }
 });
  
